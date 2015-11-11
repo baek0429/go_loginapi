dc67772dc5cc87e700f992cb9dfdc9df066f825e
baek0429 <baek0429@hotmail.com>
2015-11-11 12:19:26 +0900

    adding basic api functions (hashing, db connection, etc)

--- login.go
+++ login.go
@@ -17,16 +17,20 @@
 
 /**
  * DECRIPTION:
- * This package receives the httprequest from the client(could be mobile)
- * and returns httpresponse with appropriate information.
+ * This package receives the httprequest from the client(mobile)
+ * and returns response with appropriate information.
  * NOTICE:
  **/
 
 package login
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/nu7hatch/gouuid"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 )
 
@@ -49,6 +53,7 @@ func (srq *Request) ToString() string {
 
 // format for responseinfo
 type Response struct {
+	UniqueId    string // uuid
 	Tag         string // response
 	SuccessCode int    // 1 or 0
 }
@@ -65,6 +70,13 @@ func init() {
 // main api handler
 func handler(w http.ResponseWriter, r *http.Request) {
 
+	// setting up db
+	db, err := sql.Open("mysql", "root@cloudsql(loginapi-1125:instance-name)/dbname")
+	if err != nil {
+		fmt.Fprint(w, err)
+	}
+	defer db.Close()
+
 	// empty request and response struct address
 	var request Request
 	var response Response
@@ -73,18 +85,112 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	queried := r.URL.Query()
 
 	// assign Request parameters from quaried to local values
-	request.Tag = "Request"
+	request.Tag = queried.Get("Tag")
 	request.Username = queried.Get("Username")
 	request.Password = queried.Get("Password")
 
-	// building responses based on the db search.
+	// querying db based on the request info
 	response.Tag = "Response"
-	if (request.Username != "") || (request.Password != "") {
-		response.SuccessCode = 0
-		jsonObj, err := json.Marshal(response)
+
+	// if tag : login, register, ?
+	if request.Tag == "Login" {
+		if (request.Username != "") || (request.Password != "") {
+			// sql query for selecting user
+			sqlStmt := "SELECT unique_id, username, encrypted_password, created_at, updated_at FROM users" +
+				" WHERE username ='" + request.Username + "'"
+
+			// rows with the same request.Username
+			rows, err := db.Query(sqlStmt)
+			if err != nil {
+				fmt.Fprint(w, err)
+				return
+			}
+			defer rows.Close()
+
+			// []byte for storing the scanned values
+			var col1, col2, col3, col4, col5 []byte
+			for rows.Next() {
+				// scan the value to []byte
+				err := rows.Scan(&col1, &col2, &col3, &col4, &col5)
+				if err != nil {
+					fmt.Fprint(w, err)
+					return
+				}
+
+				// if password is right successcode= 1 else 0
+				if bcrypt.CompareHashAndPassword(col3, []byte(request.Password)) == nil {
+					response.UniqueId = string(col1)
+					if response.UniqueId != "" {
+						response.SuccessCode = 1
+					}
+				} else {
+					response.SuccessCode = 0
+				}
+			}
+		}
+	} else if request.Tag == "Register" {
+		// statment frame
+		sqlStmt := "INSERT INTO users (unique_id, username, encrypted_password) VALUES( ? , ? , ?, ?)"
+
+		// prepare statment for inserting data
+		insStmt, err := db.Prepare(sqlStmt)
 		if err != nil {
 			fmt.Fprint(w, err)
+			return
+
 		}
-		fmt.Fprintln(w, string(jsonObj[:]))
+		defer insStmt.Close()
+		var sqlValues []string
+
+		// TODO: Hashing password, assiging unique_id
+		sqlValues = GetStoreUserQuery(request.Username, request.Password)
+
+		// executes query
+		_, err = insStmt.Exec(sqlValues[0], sqlValues[1], sqlValues[2], sqlValues[3])
+		if err != nil {
+			response.SuccessCode = 0
+		} else {
+			response.SuccessCode = 1
+		}
+
+	} else {
+		response.SuccessCode = 0
+	}
+
+	// save info as json object
+	fmt.Fprint(w, response.ToString())
+	jsonObj, err := json.Marshal(response)
+	if err != nil {
+		fmt.Fprint(w, err)
+		return
+	}
+	// print json obj
+	fmt.Fprintln(w, string(jsonObj[:]))
+}
+
+// takes string var name and password
+// creates uuid, hashed password
+// returns string[] with uuid, username, hashedpassword
+func GetStoreUserQuery(name, password string) []string {
+	var result []string
+
+	u4, err := uuid.NewV4()
+	if err != nil {
+		fmt.Println("error: ", err)
+		return result
+	}
+
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	result = []string{u4.String(), name, string(hashed)}
+	return result
+}
+
+// for test only
+func ConfirmPassword(hashed, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(password))
+	if err == nil {
+		return true
+	} else {
+		return false
 	}
 }
--- login_test.go
+++ login_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestRequest(t *testing.T) {
-	req, err := http.NewRequest("GET", "http://example.com/foo?Username=baek&Password=test", nil)
+	req, err := http.NewRequest("GET", "http://example.com/foo?Username=baek&Password=test&Tag=Login", nil)
 	if err != nil {
 		t.Log(err)
 	}
@@ -15,4 +15,19 @@ func TestRequest(t *testing.T) {
 	w := httptest.NewRecorder()
 	handler(w, req)
 	t.Logf("%d - %s", w.Code, w.Body.String())
+}
+
+func TestResponse(t *testing.T) {
+}
+
+// Hash test
+func _TestGetStore(t *testing.T) {
+	var a [10]string
+	for i := 0; i < 10; i++ {
+		a[i] = GetStoreUserQuery("helloworld", "hello")[2]
+	}
+	for j := 0; j < 10; j++ {
+		t.Log(a[j])
+		t.Log(ConfirmPassword(a[j], "hello"))
+	}
 }
